bdc28edbf26eb4d9705d96a7873e0c121df7228a
hanyaenak-dothah <hanyaenak@gmail.com>
2026-05-07 02:59:42 +0700

    Add files via upload

---
+++ SMG-master/README.md
@@ -0,0 +1,20 @@
+# Info
+SimpleMaterialGenerators (SMG for short) is a Slimefun4 addon that helps aliviate some of the grind by providing simple generators that create simple materials.<br>
+
+## What it adds
+Currently it adds generators for:<br>
+Cobblestone, Stone, Smooth stone, Gravel, Sand, Glass, Netherrack and Soul sand<br>
+
+## Using the generators
+The generators by themselves will do nothing - at least not until they have a chest (directly) above them.<br>
+(There's a multiblock example in the SMG category).<br>
+Then they will start producing items.<br>
+The `Rate` of their production is measured in Slimefun `Ticks`.<br>
+
+## Downloads
+You can find the prebuilt plugin in:<br>
+https://thebusybiscuit.github.io/builds/Sefiraat/SMG/master/<br>
+
+## Plans
+There are plans to add more generators in the future (once I figure out how to do it)<br>
+Namely generators for Water, Lava and dusts.<br>
---
+++ SMG-master/src/main/java/me/waleks/simplematerialgenerators/SimpleMaterialGenerators.java
@@ -0,0 +1,53 @@
+package me.waleks.simplematerialgenerators;
+
+import io.github.thebusybiscuit.slimefun4.api.SlimefunAddon;
+import io.github.thebusybiscuit.slimefun4.libraries.dough.config.Config;
+import io.github.thebusybiscuit.slimefun4.libraries.dough.updater.GitHubBuildsUpdater;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
+public class SimpleMaterialGenerators extends JavaPlugin implements SlimefunAddon {
+
+    private static SimpleMaterialGenerators instance;
+
+    @Override
+    public void onEnable() {
+        setInstance(this);
+
+        Config cfg = new Config(this);
+
+        if (cfg.getBoolean("options.auto-update") && getDescription().getVersion().startsWith("DEV - ")) {
+            new GitHubBuildsUpdater(this, getFile(), "Sefiraat/SMG/master").start();
+        }
+
+        SMGItemSetup.setup(this);
+    }
+
+    @Override
+    public void onDisable() {
+        setInstance(null);
+    }
+
+    @Nonnull
+    @Override
+    public String getBugTrackerURL() {
+        return "https://github.com/Sefiraat/SMG/issues";
+    }
+
+    @Nonnull
+    @Override
+    public JavaPlugin getJavaPlugin() {
+        return this;
+    }
+
+    @Nonnull
+    public static SimpleMaterialGenerators getInstance() {
+        return instance;
+    }
+
+    private static void setInstance(@Nullable SimpleMaterialGenerators instance) {
+        SimpleMaterialGenerators.instance = instance;
+    }
+}
